491db23c93fcbb8355b1af59419e66d8729f58c5
Throb688 <77827171+Throb688@users.noreply.github.com>
2023-06-26 22:20:59 +0800

    Add files via upload

---
+++ src/test/java/com/itsnake/itsanke/ItsankeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsnake.itsanke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsankeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
