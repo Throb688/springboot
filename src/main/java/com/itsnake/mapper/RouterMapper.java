0eea2c4e7362eb5be3a5082863897f85351c26db
Throb688 <77827171+Throb688@users.noreply.github.com>
2023-06-26 21:54:51 +0800

    Delete src directory

--- src/main/java/com/itsnake/mapper/RouterMapper.java
+++
@@ -1,9 +0,0 @@
-package com.itsnake.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.itsnake.entity.Router;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface RouterMapper extends BaseMapper<Router> {
-}
--- src/main/java/com/itsnake/service/LogService.java
+++
@@ -1,7 +0,0 @@
-package com.itsnake.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.itsnake.entity.Log;
-
-public interface LogService extends IService<Log> {
-}
